6a2a7e7c361cea19d95842014ecceb40d09de598
Dave Draper <david.draper@alfresco.com>
2015-12-17 15:45:05 +0000

    Updates to get previews working

--- run.sh
+++ run.sh
@@ -8,5 +8,24 @@ if [ ! -f $springloadedfile ]; then
 mvn validate -Psetup
 fi
 
+echo $LD_LIBRARY_PATH | egrep "/home/dave/alfresco-5.0.2/common" > /dev/null
+if [ $? -ne 0 ] ; then
+PATH="/home/dave/alfresco-5.0.2/java/bin:/home/dave/alfresco-5.0.2/postgresql/bin:/home/dave/alfresco-5.0.2/common/bin:$PATH"
+export PATH
+LD_LIBRARY_PATH="/home/dave/alfresco-5.0.2/postgresql/lib:/home/dave/alfresco-5.0.2/common/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH
+fi
+
+##### IMAGEMAGICK ENV #####
+MAGICK_HOME="/home/dave/alfresco-5.0.2/common"
+export MAGICK_HOME
+MAGICK_CONFIGURE_PATH="/home/dave/alfresco-5.0.2/common/lib/ImageMagick-6.8.6/config-Q16"
+export MAGICK_CONFIGURE_PATH
+MAGICK_CODER_MODULE_PATH="/home/dave/alfresco-5.0.2/common/lib/ImageMagick-6.8.6/modules-Q16/coders"
+export MAGICK_CODER_MODULE_PATH
+
+GS_LIB="/home/dave/alfresco-5.0.2/common/share/ghostscript/fonts"
+export GS_LIB
+
 #MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G" mvn integration-test -Pamp-to-war
 MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G -XX:PermSize=300m" mvn integration-test -Pamp-to-war
